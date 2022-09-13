cb124b0bbae2374c1248d30887b86c12ebadb452
bishoe01 <bishoe01@gmail.com>
2022-09-13 23:59:11 +0900

    clean

--- StudyPlace/src/components/Rent/rent.jsx
+++ StudyPlace/src/components/Rent/rent.jsx
@@ -15,22 +15,10 @@ function Rent(props) {
                 <img className={styles.map} src="https://images.unsplash.com/photo-1562504208-03d85cc8c23e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
                 <img className={styles.map} src="https://ieng.hanyang.ac.kr/front/intro/location/file-load?id=21&fileId=8&mw=1280" alt="" />
             </div>
-            <h1 className={styles.title}>팀플실 예약</h1>
             <div className={styles.info}>
-                <img className={styles.roomImg} src={'/imgs/studyroom/studyroom1.jpeg'} alt="" />
-                <div className={styles.btnSeciton}>
-                    <h1 className={`${styles.btn} ${styles.maintitle}`}>팀플실</h1>
-                    <div className={styles.selection}>
-                        <button className={styles.btn} onClick={openModal}>팀플실1</button>
-                        <button className={styles.btn}>팀플실2</button>
-                        <button className={styles.btn}>팀플실3</button>
-                        <button className={styles.btn}>팀플실4</button>
-                        <button className={styles.btn}>팀플실5</button>
-                        <button className={styles.btn}>팀플실6</button>
-                    </div>
-                </div>
+                <ReserveModal/>
             </div>
-
+            
         </section>
     );
 }
@@ -41,21 +29,8 @@ export default Rent;
 function ReserveModal({ openModal }) {
     return (
         <modal className={styles.modal}>
-            <section className={styles.modalLeft}>
-                <header className={styles.modalHeader}>
-                    <h1>팀플실1</h1>
-                    <button className={styles.modalBtn} onClick={openModal}>X</button>
-                </header>
-                <img className={styles.roomState} src="/imgs/studyroom/Group.svg" alt="" />
-            </section>
-            <section className={styles.modalRight}>
-                <select>
-                    <option>인원수 선택</option>
-                    <option value="1">2인</option>
-                    <option value="2">3인</option>
-                    <option value="3">4인</option>
-                    <option value="4">4~6인</option>
-                </select>
+            <section className={styles.modal__room}>
+                
             </section>
         </modal>
     );
--- StudyPlace/src/components/footer/footer.jsx
+++ StudyPlace/src/components/footer/footer.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './footer.module.css';
 function Footer(props) {
     return (
         <footer className={styles.footer}>
-            <h2 className={styles.title}>Contact</h2>
-            <div className={styles.logoContainer}>
+            <section className={styles.leftContainer}>
+                <h2 className={styles.h2}>HY-Together</h2>
+                <p>
+                    <h6 className={styles.tel}>TEL +82-10-3733-0690</h6>
+                    <h6 className={styles.email}>Email : contact@hanyang.ac.kr</h6>
+                </p>
+                <span className={styles.copyright}>Copyright 2022 Hanyang Univ. All Rights Reserved.</span>
+            </section>
+            <section className={styles.rightContainer}>
+                <ul className={styles.links} >
+                    <li><Link to="/" className={styles.navbtn} onClick={() => { }}>HOME</Link></li>
+                    <li><Link to="/about" className={styles.navbtn}>ABOUT</Link></li>
+                    <li><Link to="/profile" className={styles.navbtn}>PROFILE</Link></li >
+                    <li><Link to="/settings" className={styles.navbtn} >SETTINGS</Link></li>
+                </ul>
+                <div className={styles.logos}>
                 <i className={`fa-brands fa-github ${styles.logo}`}></i>
-                <i className={`fa-solid fa-comment-dots ${styles.logo}`}></i>
-                <i className={`fa-brands fa-instagram ${styles.logo}`}></i>
-            </div>
-            <span className={styles.number}>010-xxxx-xxxx</span>
-            <i className="fa-solid fa-phone"></i>
-            
+                    <i className={`fa-solid fa-comment-dots ${styles.logo}`}></i>
+                    <i className={`fa-brands fa-instagram ${styles.logo}`}></i>
+                    <i className={`fa-brands fa-discord ${styles.logo}`}></i>
+                    <i className={`fa-brands fa-facebook ${styles.logo}`}></i>
+                    </div>
+            </section>
         </footer>
     );
 }
 export default Footer;
+
