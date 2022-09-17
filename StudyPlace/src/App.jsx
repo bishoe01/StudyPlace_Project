7c270ba5821b58f9a4f906bc9eea7130feef53bf
bishoe01 <bishoe01@gmail.com>
2022-09-17 21:24:52 +0900

    carousel antDesign add

--- StudyPlace/src/App.jsx
+++ StudyPlace/src/App.jsx
@@ -11,9 +11,11 @@ function App() {
     "경상대학" : {
     department:["경제학부", "경영학부" , "보험계리학과", "회계세무학과"],
     room : [1,2,3,4,5,6,7,8],
-    type : ["study","meeting","seminar"],
+    type : ["스터디","회의","팀플", "투명벽", "4인추천"],
     people : [2,3,4,5,6],
-    img : "",},
+    description : "투명한 벽으로 구분된 경상대학 팀플실",
+    img : ["https://images.unsplash.com/photo-1513694203232-719a280e022f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
+    "https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1258&q=80"],},
     "소프트웨어융합대학" : 
     {
     department:["소프트웨어학부(컴퓨터학부)", "ICT융합학부"],
--- StudyPlace/src/components/Rent/rent.jsx
+++ StudyPlace/src/components/Rent/rent.jsx
@@ -3,27 +3,47 @@ import React, { useEffect, useState } from 'react';
 import { Carousel } from 'antd';
 import styles from './rent.module.css';
 import { useNavigate } from 'react-router-dom';
-function Rent({roomInfo}) {
+import { Divider, Tag } from 'antd';
+function Rent({ roomInfo }) {
     const 경상대학 = roomInfo["경상대학"];
-    console.log(경상대학);
-    return(
-    <section className={styles.rent}>
-        <roomInterFace roomData={경상대학}/>
-        <h1 style={{fontSize:"42px"}}>경상대학-팀플실</h1>
-        <h5 style={{fontSize:"22px"}}>다목적-회의,스터디,자습(2인~6인)</h5>
-        
-    </section>
+    const 소프트웨어융합대학 = roomInfo["소프트웨어융합대학"];
+    return (
+        <section className={styles.rent}>
+            <RoomInterFace title={"경상대학-팀플실"} roomData={경상대학} />
+        </section>
     );
 }
 export default Rent;
 
-function roomInterFace({roomData}){
-    const room__department = roomData.department;
-    console.log(room__department);
-    return(
+function RoomInterFace({ roomData, title }) {
+    const Department = roomData.department;
+    const Type = roomData.type;
+    const room = roomData.room;
+    const description = roomData.description;
+    const img = roomData.img;
+
+    return (
         <>
-        <h1>{roomData}</h1>
-        room
+            <section className={styles.headerSection}>
+                <h1 style={{ fontSize: "42px", fontWeight: "400w" }}>{title}</h1>
+                <p style={{ color: "lightgray", fontSize: "22px" }}>{description}</p>
+                {Type.map((type) =>
+                    <Tag className={styles.tag} style={{ borderRadius: "20px", fontSize: "14px" }} color='#2db7f5'>
+                        {`#${type}`}</Tag>)}
+            </section>
+            <div className={styles.box}>
+                <section className={styles.left__introduce}>
+                    <Carousel autoplay effect="fade" className={styles.ImageSection}>
+                        {img.map((img) =>
+                        (
+                            <div className={styles.carousel}><img src={img} />
+                            </div>))}
+                    </Carousel>
+                </section>
+                <section className={styles.right__rent}>
+                    <h6 className={styles.right__title}>세부사항 선택</h6>
+                </section>
+            </div>
         </>
     );
 }
