b271b0714d95e2e7e76038776ba4a785223b55ea
park4608 <dusgml4608@naver.com>
2022-09-18 16:43:02 +0900

    프로필 레이아웃 수정

--- StudyPlace/src/App.jsx
+++ StudyPlace/src/App.jsx
@@ -2,30 +2,33 @@ import './App.css';
 import Department from './components/department/department';
 import Footer from './components/footer/footer';
 import Navigation from './components/navigation/navigation';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom';
 import Rent from './components/Rent/rent';
-import Profile from './components/Profile/profile'
+import Profile from './components/Profile/profile';
 import Notice from './components/Notice/notice';
+import Posting from './components/Posting/posting';
 import { Profiler, useState } from 'react';
 function App() {
-  const [user, setuser] = useState([{
-    id :1 ,
-    name : "Kim",
-    age: 23,
-    department : "Software",
-    grade : 3,
-  }]);
+  const [user, setuser] = useState([
+    {
+      id: 1,
+      name: 'Kim',
+      age: 23,
+      department: 'Software',
+      grade: 3,
+    },
+  ]);
   return (
     <>
-    <Navigation/>
-    <Routes>
-        <Route path="/" element={<Department/>} />
-        <Route path="engineer" element={<Rent/>} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/notice" element={<Notice/>} />
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<Department />} />
+        <Route path='engineer' element={<Rent />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/notice' element={<Notice />} />
+        <Route path='/posting' element={<Posting />} />
       </Routes>
-    <Footer/>
-
+      <Footer />
     </>
   );
 }
--- StudyPlace/src/components/Profile/profile.jsx
+++ StudyPlace/src/components/Profile/profile.jsx
@@ -11,55 +11,62 @@ function Profile(props) {
 
   return (
     <div className={styles.container}>
-      <section className={styles.box}>
-        <div className={styles.imgBox}>
-          <img className={styles.profileImg} src='/imgs/HanyangE/defaultImg.png' alt='' />
+      <section className={`${styles.box} ${styles.profile}`}>
+        <div className={styles.boxInner}>
+          <div className={styles.imgBox}>
+            <img className={styles.profileImg} src='/imgs/HanyangE/defaultImg.png' alt='' />
+          </div>
+          {/* <div className={styles.imgBtnBox}>
+            <button className={styles.imgBtn}>이미지 업로드</button>
+            <button className={styles.imgBtn}>이미지 제거</button>
+          </div> */}
+          <div className={styles.userNameBox}>
+            <span className={styles.name}>하냥이</span>
+            {/* <button className={styles.modifyBtn}>수정</button> */}
+          </div>
         </div>
-        <div className={styles.imgBtnBox}>
-          <button className={styles.imgBtn}>이미지 업로드</button>
-          <button className={styles.imgBtn}>이미지 제거</button>
-        </div>
-        <div className={styles.userNameBox}>
-          <span className={styles.name}>하냥이</span>
-          <button className={styles.modifyBtn}>수정</button>
+        <div className={styles.verticalLine}></div>
+        <div className={styles.boxInner}>
+          <ul className={styles.info}>
+            <li>
+              <div className={`${styles.infoItem} ${`icon-mail`}`}>
+                <span className={styles.infoText}>hanyang@hanyang.ac.kr</span>
+              </div>
+            </li>
+            <li>
+              <div className={`${styles.infoItem} ${`icon-building`}`}>
+                <span className={styles.infoText}>2022018493</span>
+              </div>
+            </li>
+            <li>
+              <div className={`${styles.infoItem} ${`icon-building`}`}>
+                <span className={styles.infoText}>소프트웨어융합대학</span>
+              </div>
+            </li>
+            <li>
+              <div className={`${styles.infoItem} ${`icon-building`}`}>
+                <span className={styles.infoText}>소프트웨어학부 소프트웨어전공</span>
+              </div>
+            </li>
+            <li>
+              <div className={`${styles.infoItem} ${`icon-lock`}`}>
+                <span className={styles.infoText}>비밀번호 변경</span>
+                <button className={`${styles.modifyBtn} ${styles.liBtn}`}>수정</button>
+              </div>
+            </li>
+          </ul>
         </div>
       </section>
-      <section className={styles.box}>
-        <ul className={styles.info}>
-          <li>
-            <div className={`${styles.infoItem} ${`icon-mail`}`}>
-              <span className={styles.infoText}>hanyang@hanyang.ac.kr</span>
-              <button
-                className={`${styles.modifyBtn} ${styles.liBtn}`}
-                onClick={() => {
-                  setEmailModal(true);
-                }}>
-                수정
-              </button>
-            </div>
-          </li>
-          <li>
-            <div className={`${styles.infoItem} ${`icon-building`}`}>
-              <span className={styles.infoText}>소프트웨어학부</span>
-              <button
-                className={`${styles.modifyBtn} ${styles.liBtn}`}
-                onClick={() => {
-                  setDeptModal(true);
-                }}>
-                수정
-              </button>
-            </div>
-          </li>
-          <li>
-            <div className={`${styles.infoItem} ${`icon-lock`}`}>
-              <span className={styles.infoText}>비밀번호 변경</span>
-              <button className={`${styles.modifyBtn} ${styles.liBtn}`}>수정</button>
-            </div>
-          </li>
-        </ul>
-      </section>
-      <section className={styles.box}>
+
+      <section className={`${styles.box} ${styles.reservation}`}>
         <h3>예약 내역</h3>
+        <div className={styles.resvWarp}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat impedit earum architecto. Voluptatem, numquam in excepturi consequatur fuga obcaecati aspernatur voluptas amet ex hic sint
+            qui! Facilis ratione deleniti pariatur. Eius st rerum odit ratione amet pariatur quae id, hic, placeat non voluptas quibusdam adipisci? Quibusdam, ad nulla? Minima aliquid, eos consectetur
+            corrupti maiores rem laboum exepturi necessitatibus natus error?
+          </p>
+        </div>
       </section>
       {emailModal ? <EmailModal emailModal={emailModal} setEmailModal={setEmailModal} /> : null}
       {deptModal ? <DeptModal deptModal={deptModal} setDeptModal={setDeptModal} /> : null}
