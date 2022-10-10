efee0c2cfe5a6ce21ccfd23da8cc1c559e4a9333
bishoe01 <bishoe01@gmail.com>
2022-10-10 22:18:41 +0900

    new category select

--- StudyPlace/src/App.jsx
+++ StudyPlace/src/App.jsx
@@ -12,6 +12,7 @@ import { Zoom } from 'react-reveal';
 
 function App() {
   const BuildingList = [
+    "학술정보관",
     "공학대학",
     "소프트웨어융합대학",
     "과학기술대학",
@@ -31,7 +32,7 @@ function App() {
         department: ["경제학부", "경영학부", "보험계리학과", "회계세무학과"],
         room: [1, 2, 3, 4, 5, 6, 7, 8],
         type: ["스터디", "회의", "팀플", "투명벽", "4인추천"],
-        people: [4, 6],
+        people: [2,3,4,5,6],
         description: "투명한 벽으로 구분된 경상대학 팀플실",
         detail: `경상대학 팀플실은 \n 하루 두시간까지만 예약이 가능합니다.`,
         img: ["https://images.unsplash.com/photo-1513694203232-719a280e022f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
@@ -55,7 +56,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Department />} />
         <Route path="engineer" element={<Rent roomInfo={roomInfo} />} />
-        <Route path="software" element={<Category BuildingList={BuildingList}/>} />
+        <Route path="software" element={<Category BuildingList={BuildingList} roomInfo={roomInfo}/>} />
       </Routes>
       <Footer />
 
--- StudyPlace/src/components/category.jsx
+++ StudyPlace/src/components/category.jsx
@@ -2,41 +2,66 @@ import React, { useState } from 'react';
 import { Radio } from 'antd';
 import '../App.scss';
 import { Fade, Zoom } from 'react-reveal';
-function Category({BuildingList}) {
+function Category({ BuildingList, roomInfo }) {
     const [value, setValue] = useState(1);
     const onChange = (e) => {
         console.log('radio checked', e.target.value);
         setValue(e.target.value);
     };
+    const [tabRoom, setTabRoom] = useState("경상대학");
+    const showRoom = (roomName) => {
+        setTabRoom(roomName);
+    }
+    const [clickBtn, setClickBtn] = useState(false);
+
     return (
         <Fade top>
-        <section className='category'>
-            <h1 className='h1'>카테고리</h1>
-            <div className='category__check'>
-                <div className='check__title'>
-                    <span>검색모드</span>
-                </div>
-                <div className='check__list'>
-                    <Radio.Group onChange={onChange} value={value} className="list__radio">
-                        <Radio className='radio' value={1}>전체항목</Radio>
-                        <Radio className='radio' value={2}>소속대학만</Radio>
-                    </Radio.Group>
-                </div>
+
+            <div>
+                <section className='category'>
+                    <h1 className='h1'>카테고리</h1>
+                    <div className='category__check'>
+                        <div className='check__title'>
+                            <span>검색모드</span>
+                        </div>
+                        <div className='check__list'>
+                            <Radio.Group onChange={onChange} value={value} className="list__radio">
+                                <Radio className='radio' value={1}>전체항목</Radio>
+                                <Radio className='radio' value={2}>소속대학만</Radio>
+                            </Radio.Group>
+                        </div>
+                    </div>
+                    <div className='selection'>
+                        <div className='building'>
+                            {BuildingList.map((item, index) => {
+                                return (
+                                    <button onClick={() => showRoom(item)}
+                                        style={{ backgroundColor: item===tabRoom ? "#5A8DFF" : " " }}>{item}</button>
+                                )}
+                            )}
+                        </div>
+                        <div className='number'>
+                            {
+                                roomInfo[tabRoom].room.map((item, index) => {
+                                    return (
+                                        <button>{item}</button>
+                                    )})
+                            }
+                        </div>
+                        <div className='people'>
+                            {
+                                roomInfo[tabRoom].people.map((item, index) => {
+                                    return (
+                                        <button>{`${item}인`}</button>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </section>
             </div>
-            <div className='selection'>
-                <div className='people'>
-                {BuildingList.map((item, index) => {
-                    return(
-                        <button>{item}</button>
-                    )
-                }
-                )}
-                </div>
-                <div className='number'></div>
-                <div className='time'></div>
-            </div>
-        </section>
         </Fade>
+
     );
 }
 
@@ -44,10 +69,19 @@ export default Category;
 
 
 
-function CategoryBtn({name}) {
+function CategoryBtn({ name }) {
     return (
         <div>
             <button>{name}</button>
         </div>
     );
+}
+
+
+function RoomTab({ number }) {
+    return (
+        <div>
+            <button>{number}</button>
+        </div>
+    )
 }
